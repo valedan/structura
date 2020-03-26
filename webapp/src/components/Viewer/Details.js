e93c242f1a31df6b12df5f9f98ef3709dd36c880
Dan <dan@danmolloy.dev>
2020-03-26 18:56:30 -0400

    align viewer details title

--- webapp/src/components/Viewer/Details.js
+++ webapp/src/components/Viewer/Details.js
@@ -9,45 +9,48 @@ import TableBody from "@material-ui/core/TableBody";
 const Details = ({ selected }) => {
   return (
     <Wrapper elevation={2}>
-      <Title>{selected ? `Micrograph ${selected.uid}` : "Loading..."}</Title>
-      {selected && (
-        <StatsTable size="small" padding="none">
-          <TableBody>
-            <TableRow>
-              <TableCell>Number of Picks:</TableCell>
-              <TableCell>{selected.num_particles}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Motion Curvature:</TableCell>
-              <TableCell>{selected.motion_curvature.toFixed(3)}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Total Motion:</TableCell>
-              <TableCell>{selected.motion_total_pix.toFixed(3)}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Pick Power Median:</TableCell>
-              <TableCell>{selected.pick_pow_median.toFixed(3)}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Pick NCC Median:</TableCell>
-              <TableCell>{selected.pick_ncc_median.toFixed(3)}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>CTF Fit to Å:</TableCell>
-              <TableCell>{selected.ctf_fit_to_A.toFixed(3)}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Defocus:</TableCell>
-              <TableCell>{selected.df_avg.toFixed(3)}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Astigmatism:</TableCell>
-              <TableCell>{selected.df_ast.toFixed(3)}</TableCell>
-            </TableRow>
-          </TableBody>
-        </StatsTable>
-      )}
+      <TextWrapper>
+        <Title>{selected ? `Micrograph ${selected.uid}` : "Loading..."}</Title>
+        {selected && (
+          <StatsTable size="small" padding="none">
+            <TableBody>
+              <TableRow>
+                <TableCell>Number of Picks:</TableCell>
+                <TableCell>{selected.num_particles}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Motion Curvature:</TableCell>
+                <TableCell>{selected.motion_curvature.toFixed(3)}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Total Motion:</TableCell>
+                <TableCell>{selected.motion_total_pix.toFixed(3)}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Pick Power Median:</TableCell>
+                <TableCell>{selected.pick_pow_median.toFixed(3)}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Pick NCC Median:</TableCell>
+                <TableCell>{selected.pick_ncc_median.toFixed(3)}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>CTF Fit to Å:</TableCell>
+                <TableCell>{selected.ctf_fit_to_A.toFixed(3)}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Defocus:</TableCell>
+                <TableCell>{selected.df_avg.toFixed(3)}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Astigmatism:</TableCell>
+                <TableCell>{selected.df_ast.toFixed(3)}</TableCell>
+              </TableRow>
+            </TableBody>
+          </StatsTable>
+        )}
+      </TextWrapper>
+
       {selected && <Image src={`api/image?filename=${selected.fname}`}></Image>}
     </Wrapper>
   );
@@ -63,16 +66,17 @@ const Wrapper = styled(Paper)`
   height: calc(100vh - 60px - 4rem);
 `;
 
-const Title = styled.h1`
-  text-align: center;
+const TextWrapper = styled.div`
+  width: 280px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 2rem;
 `;
+
+const Title = styled.h1``;
 
 const StatsTable = styled(Table)`
   && {
-    width: 260px;
-    margin-bottom: 2rem;
-    margin-left: auto;
-    margin-right: auto;
     .MuiTableCell-root {
       border-bottom: none;
     }
